97ca70b4e3f30233a67669c1dbba461a18e3e6ef
budocf <budocf@.rose-hulman.edu>
2015-04-30 14:48:59 -0400

    initial git commit of makerLab website

---
+++ RoseMakerSpace/Controllers/ResourcesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoseMakerSpace.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View("FAQ");
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
---
+++ RoseMakerSpace/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoseMakerSpace.MvcApplication" Language="C#" %>

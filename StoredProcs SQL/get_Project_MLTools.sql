4f08f8589e56506b5ea794c95667960c00c9fa83
Doolan <doolansr@rose-hulman.edu>
2015-05-18 23:59:39 -0400

    three get procs

---
+++ StoredProcs SQL/get_Project_MLTools.sql
@@ -0,0 +1,42 @@
+USE [333_MakerLab];
+
+IF OBJECT_ID('[dbo].[get_Project_MLTools]') IS not NULL
+    DROP PROCEDURE [dbo].get_Project_MLTools
+GO
+
+CREATE PROCEDURE get_Project_MLTools
+
+(@Project_ID INT)
+
+AS
+SET NOCOUNT ON
+
+SELECT tool.Name, 1 AS [FLAG] 
+	FROM Maker_Lab_Tool tool 
+	JOIN Project_MLTool ON ID = MLTool_ID
+	WHERE Project_MLTool.Project_ID = @Project_ID
+
+UNION 
+
+SELECT tool.Name, 0 AS [FLAG] 
+	FROM Maker_Lab_Tool tool
+	WHERE ID NOT IN(
+		SELECT MLTool_ID FROM Project_MLTool
+		WHERE Project_MLTool.Project_ID = @Project_ID)
+
+--ERROR Stuff
+DECLARE @Status1 SMALLINT
+SET @Status1 = @@ERROR
+IF @Status1 <> 0 
+	BEGIN
+	-- Return error code to the calling program to indicate failure. 
+	PRINT 'An error occurred creating the project'
+	RETURN(@Status1)
+END
+ELSE 
+	BEGIN
+	RETURN 0
+END
+GO
+
+--EXEC get_user_Skills 800983530
